00a6d64fa121891cf1dc0b3fda367c1d740e231c
arshad.ansari <arshad.ansari@globant.com>
2024-02-06 13:11:06 +0530

    second commit

--- Python_small_snippets/string_manipulation.py
+++ Python_small_snippets/string_manipulation.py
@@ -1,5 +1,5 @@
 str1 = "Hello"
-
+str4 = "Arshad! {}"
 str2 = str1.upper()
 print(str2)
 
@@ -7,3 +7,6 @@ str3 = str1.isupper()
 print(str3)
 
 print(str1 == str2)
+
+str5 = str1 + " " + str4.format("this is going to be a good day!!!")
+print(str5)
