f2911f138d8f59a58ef8d7cd56881767d3ca9dcd
arshad.ansari <arshad.ansari@globant.com>
2024-02-07 14:33:36 +0530

    i did some small change

---
+++ python_codes/test_something.py
@@ -0,0 +1,16 @@
+str1 = "Hello"
+str6 = "Hi world"
+str4 = "Arshad! {}"
+str2 = str1.upper()
+print(str2)
+
+str3 = str1.isupper()
+print(str3)
+
+print(str1 == str2)
+
+str7 = str1 + str6
+print(str7)
+
+str5 = str1 + " " + str4.format("this is going to be a good day!!!")
+print(str5)
